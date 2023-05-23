f06be7bc63ff12173b7900d1e8d7f2dba543d0a7
Pedri <128613849+nrgsnoopy8@users.noreply.github.com>
2023-05-23 09:05:16 -0600

    Comparacion con numeros

---
+++ js/main.js
@@ -0,0 +1,33 @@
+console.log("Hello World!");
+
+//alert("hoy le gano a leo")
+
+/*
+Comentario Multilinea
+
+let nombre=prompt("Mi nombre es?");
+
+console.log(nombre);
+
+console.log(parseInt(nombre));
+
+console.log(typeof(nombre));
+*/
+let numeroUno = prompt("Ingresar un numero");
+let numeroDos = prompt("Ingresar otro numero");
+
+let resultadoSuma = parseInt (numeroUno) + parseInt(numeroDos);
+
+console.log(`El resultado de la suma es:  ${resultadoSuma}`);
+
+let resultadoResta = parseInt (numeroUno) - parseInt(numeroDos);
+
+console.log(`El resultado de la resta es:  ${resultadoResta}`);
+
+let resultadoMultiplicacion = parseInt (numeroUno) * parseInt(numeroDos);
+
+console.log(`El resultado de la multiplicacion es:  ${resultadoMultiplicacion}`);
+
+let resultadoDivision = parseInt (numeroUno) / parseInt(numeroDos);
+
+console.log(`El resultado de la division es:  ${resultadoDivision}`);
