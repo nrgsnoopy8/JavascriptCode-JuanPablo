671bf6566d7d35800b055cc0ee8564cbc8a0d834
Pedri <128613849+nrgsnoopy8@users.noreply.github.com>
2023-05-31 09:12:06 -0600

    Avance

---
+++ js/edadEnDias.js
@@ -0,0 +1,7 @@
+function Edadendias(){
+
+  prompt("Ingresar tu fecha de nacimiento");
+  let nacimiento = parseint (prompt(`Ingresa tu fecha de nacimiento`));
+  let edad = (2023 - nacimiento)*365;
+}
+    
--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ console.log(nombre);
 console.log(parseInt(nombre));
 
 console.log(typeof(nombre));
-*/
+
 let numeroUno = prompt("Ingresar un numero");
 let numeroDos = prompt("Ingresar otro numero");
 
@@ -31,3 +31,40 @@ console.log(`El resultado de la multiplicacion es:  ${resultadoMultiplicacion}`)
 let resultadoDivision = parseInt (numeroUno) / parseInt(numeroDos);
 
 console.log(`El resultado de la division es:  ${resultadoDivision}`);
+}
+*/
+
+//Datos dos numeros identificar cual numero es mayor
+//Cual es menor o si son iguales
+
+/*
+let numeroUno = prompt("Ingresar un numero");
+let numeroDos = prompt("Ingresar otro numero");
+
+if(numeroUno>numeroDos){
+console.log(`El numero${numeroUno}es mayor que${numeroDos}`);
+
+}else if(numeroUno<numeroDos){
+    console.log(`El numero${numeroDos}es mayor que${numeroUno}`);
+}else if(numeroUno<numeroDos){
+    console.log(`El numero${numeroUno}es mayor que${numeroDos}`);
+}
+*/
+/*
+//Dado un numero identificado si un numero es par o impar
+
+let numeroUno = prompt("Ingresar el  valor deseado");
+let resultado = parseInt("numeroUno")%2;
+if  (resultado==0){
+  // console.log(`El numero${numeroUno} es par`);
+}else{
+    console.log(`El numero ${numeroUno}es impar`);
+}*/
+
+
+
+
+}
+
+
+
