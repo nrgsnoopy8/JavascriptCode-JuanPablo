2971987e841d876cd747652eb3ccfc5af34d31a9
Pedri <128613849+nrgsnoopy8@users.noreply.github.com>
2023-06-02 09:59:01 -0600

    Edad en dias

--- js/edadEnDias.js
+++ js/edadEnDias.js
@@ -1,7 +1,19 @@
 function Edadendias(){
 
   prompt("Ingresar tu fecha de nacimiento");
-  let nacimiento = parseint (prompt(`Ingresa tu fecha de nacimiento`));
+  let nacimiento = parseInt (prompt(`Ingresa tu fecha de nacimiento`));
   let edad = (2023 - nacimiento)*365;
+  
+  let respuesta = document.createTextNode(`Tu edad en dias es : ${edad}`);
+  let h2 = document.createElement("h2");
+
+  h2.setAttribute("id","Edadendias");
+
+  h2.appendChild(respuesta);
+
+  document.getElementById("resultado").appendChild(h2);
+
 }
-    
+function reset(){
+document.getElementsById("resultado").remove();
+}    
